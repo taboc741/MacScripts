028f6b0c9f505b947d9d5ef0166b072afb1eaf02
taboc741 <34924920+taboc741@users.noreply.github.com>
2020-12-22 16:11:36 -0500

    Update SoftwareUpdate4-beta.sh
    #1 fix, support for Big Sur now works based on my testing.
        -no longer doing special reboot based on if the update requires shutdown or not.  Big Sur doesn't honor this behavior in the script and thus some logs will be lost but there isn't much for it.
    #2 Cleaned up logging grammar and other spelling errors
    #3 Removed redundant messaging around starting to install updates.  Feedback says that prompt is more annoying than useful.

--- A-Kinder-macOS-Update/SoftwareUpdate4-beta.sh
+++ A-Kinder-macOS-Update/SoftwareUpdate4-beta.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ##  SoftwareUpdate4-beta.sh
-##  Created by Chris on 5/31/18. Last edited by Chris 4/15/2019
+##  Created by Chris on 5/31/18. Last edited by Chris 12/20/2020
 ##### Special thanks to mm2270, cstout, nvandam from JamfNation for helping me with some testing and code suggestions to problems I couldn't solve on my own.#####
 
 ######### Create settings for logging and create log file #########
@@ -43,7 +43,7 @@ fi
 ## If there are no system updates, quit
 if [[ $updatesNoRestart = "none" && $restartRequired = "none" ]]; then
     sendToLog "No updates at this time, updating Jamf inventory"
-    jamf policy -trigger recon
+    jamf recon
     sendToLog "Inventory update complete, script exit."
     exit 0
 fi
@@ -52,20 +52,13 @@ sendToLog "Updates found."
 ## Download all updates before trying to install them to make a smoother user experiance.
 title='Software Update Required'
 heading='Software updates'
-description='Downloading the required Software updates. This prompt will change when it is time to install them. It is safe to continue using the mac while this happens.'
+description='Downloading and installing the required Apple updates. It is safe to continue using the mac while this happens.'
 button1="Ok"
 ##prompt the user
 "/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" -windowType hud -title "$title" -heading "$heading" -alignHeading justified -description "$description" -alignDescription left -icon "$icon" -button1 "$button1" -lockHUD > /dev/null 2>&1 &
 softwareupdate --download --all --force --no-scan
-killall jamfHelper
-##Map some variables to pretty up the command to prompt the user
-title='Software Update Required'
-heading='Required Software update'
-description='The Apple Updates are ready to install. Please press start.'
-button1="Start Updates"
-##prompt the user
-prompt=`"/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper" -windowType hud -title "$title" -heading "$heading" -alignHeading justified -description "$description" -alignDescription left -icon "$icon" -button1 "$button1" -timeout 14400 -countdown -lockHUD`
-sendToLog "prompt equaled $prompt. 0=start 1=failed to prompt 2=canceled 239=exited"
+
+##Install updates now that they are downloaded.
 ##trap the output of the softwareupdate command to a variable to be grepped for the words Shut Down
 sendToLog "Attempting to install updates"
 appliedUpdates=`softwareupdate --install --all --force && sendToLog "Updates Applied"`
@@ -74,10 +67,11 @@ sendToLog "$appliedUpdates"
 ##parse $appliedUpdates looking for shut down
 shutDownRequired=`echo $appliedUpdates | grep "shut down" | grep -v '\*' | cut -d , -f 1`
 [[ -z $shutDownRequired ]] && shutDownRequired="none"
+killall jamfHelper
 
 ######### Check if any restart is required and if not exit the update script #########
 if [[ $restartRequired = "none" && $shutDownRequired = "none" ]]; then
-    sendToLog "no reboot required, Alerting user Updates complete"
+    sendToLog "no reboot required, alerting user updates are complete"
 	##Map some variables to pretty up the command to prompt the user
 	title='Software Update'
 	heading='Software update'
@@ -103,16 +97,11 @@ if [[ $restartRequired != "none" || $shutDownRequired != "none" ]]; then
 	##Map some variables to pretty up the command to prompt the user
 	title='Software Update Required'
 	heading='Required Mac OS update'
-	description='We are now updating your Mac System software. These updates should not take longer than 30 to 45 minutes depending on how many updates your Mac needs. If you see this screen for more than 45 minutes please call Service Desk. Your machine may reboot or shutdown.  Please do not manually turn off this computer. This message will go away when updates are complete.'
+	description='Your Mac is now updating your Apple software. These updates should not take longer than 30 to 45 minutes depending on how many updates your Mac needs. If you see this screen for more than 45 minutes please call Service Desk. Your machine may reboot or shutdown.  Please do not manually turn off this computer. This message will go away when updates are complete.'
 	button1="Reboot"
+    ##This jamfHelper window will persist after the script exits intentionally so users will be less confused about what is going on.
     /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType hud -lockHUD -heading "$heading" -description "$description" -icon "$icon" > /dev/null 2>&1 &
 fi
-if [[ $shutDownRequired != "none" ]]; then
-    sendToLog "Shutdown is required.  Issuing system halt. Attempting to save log to Jamf"
-    sleep 30 && /sbin/halt & exit 0
-else
-    sendToLog "starting reboot"
-    ## using the Jamf restart process because it increases the odds of Jamf receiveing the logs back from the completed policy.
-    shutdown -r +2 & sendToLog "Script exit"
-    exit 0
-fi
+##Trigger reboot by using the softwareupate --restart flag.  This may result in the reboot happening faster than jamf can exit the script and logs be uploaded to Jamf.  The --restart flag will correctly reboot regardless of if a shutdown for T2 update or a regular reboot is required.
+sendToLog "Reboot is required. Attempting to save log to Jamf"
+softwareupdate --install --restart --all && exit 0
